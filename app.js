4cb18e3a70b16b5fa3155126c029130a4f8a05a1
THXBurke <rachelwheels@gmail.com>
2017-05-20 13:46:47 -0700

    clean up rename

---
+++ app.js
@@ -0,0 +1,13 @@
+function Kindergarten(childName, parentName, forest, beach, treehouse, present) {
+  this.childName = childName;
+  this.parentName = parentName;
+  this.forest = forest;
+  this.beach = beach;
+  this.treehouse = treehouse;
+  this.present = present;
+  this.checkLocation = function() {
+    if(document.getElementById('radio1a').onclick === true;) {
+      console.log(childName + 'is at ' );
+    }
+  }
+}
