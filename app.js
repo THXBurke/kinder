6fa3fd18d207b109ca54bf4f6529b6d139736a3a
THXBurke <rachelwheels@gmail.com>
2017-05-20 20:34:31 -0700

    add form

--- app.js
+++ app.js
@@ -1,21 +1,53 @@
-// 'use strict'
+'use strict'
 ///global variables
-var childArray = [];
+var childArray = ['child1', 'child2', 'child3', 'child4', 'child5', 'child6', 'child7', 'child8', 'child9', 'child10', 'child11', 'child12'];
+var totalChildren = 0;
+var locationsArray = [];
+
+var beachLocation = document.getElementById()
+function ChildLocation() {
 
 
-function checkChildLocation() {
 
-  if(document.getElementById('radio1beach').checked === true) {
-    document.write('beach');
-  }else if(document.getElementById('radio1treehouse').checked === true) {
-    console.log('treehouse');
-  } else if (document.getElementById('radio1forest').checked === true) {
-    console.log('forest');
-  } else {
-    //console.log('no location selected');
-  }
-}
 
-function uncheck() {
-    document.getElementById('radio1beach').checked === false;
-}
+
+
+
+
+
+
+
+
+
+
+
+//   if(document.getElementById('radio1beach').click === true) {
+//     console.log('beach');
+//   }else if(document.getElementById('radio1treehouse').click === true) {
+//     console.log('treehouse');
+//   } else if (document.getElementById('radio1forest').click === true) {
+//     console.log('forest');
+//   } else {
+//     //console.log('no location selected');
+//   }
+// }
+//
+// function uncheck() {
+//     document.getElementById('radio1beach').click === false;
+// }
+//
+// window.onload = function() {
+//
+//         var child1 = document.getElementById('radio1beach');
+//         var child2 = document.getElementById('radio2beach');
+//         var child3 = document.getElementById('radio3beach');
+//
+//         child1.onclick = handler;
+//         child2.onclick = handler;
+//         child3.onclick = handler;
+//
+//     }
+//
+//     function handler() {
+//         console.log('clicked');
+//     }
