9da92769cf32fcb366ed5a2c43281a6146cbc345
THXBurke <rachelwheels@gmail.com>
2017-05-20 17:03:59 -0700

    consider functionality

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-function Kindergarten(childName, parentName, forest, beach, treehouse, present) {
-  this.childName = childName;
-  this.parentName = parentName;
-  this.forest = forest;
-  this.beach = beach;
-  this.treehouse = treehouse;
-  this.present = present;
-  this.checkLocation = function() {
-    if(document.getElementById('radio1a').onclick === true;) {
-      console.log(childName + 'is at ' );
-    }
+// 'use strict'
+///global variables
+var childArray = [];
+
+
+function checkChildLocation() {
+
+  if(document.getElementById('radio1beach').checked === true) {
+    document.write('beach');
+  }else if(document.getElementById('radio1treehouse').checked === true) {
+    console.log('treehouse');
+  } else if (document.getElementById('radio1forest').checked === true) {
+    console.log('forest');
+  } else {
+    //console.log('no location selected');
   }
+}
+
+function uncheck() {
+    document.getElementById('radio1beach').checked === false;
 }
