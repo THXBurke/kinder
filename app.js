6a90b6dafcb4756ac365b260bbe3398326a27751
THXBurke <rachelwheels@gmail.com>
2017-05-21 21:56:36 -0700

    table functionality

--- app.js
+++ app.js
@@ -1,69 +1,92 @@
 'use strict'
 ///global variables
 var childArray = [];
-var totalChildren = 0;
-var locationsArray = ['Beach', "Treehouse", "Forest"];
-
-
-var addChildInfo = document.getElementById('addChildInfo');
+var totalChildrenPerLocation = 0;
+var locationsArray = ['Beach', 'Treehouse', 'Forest'];
+// var staffArray = 0;
+var addChildForm = document.getElementById('addChildForm');
+var addChildInfoTable = document.getElementById('addChildInfoTable');
 //var staffLocation = document.getElementById('addStaffLocation');
 
-
-function DailyData(childName, location, staff) {
+//DailyData constructor
+function ChildDailyData(childName, location, staff) {
   this.childName = childName;
   this.location = location;
   this.staff = staff;
 }
 
-this.render = function() {
-  var rowElement = document.createElement('tr');
-  var dataElement = document.createElement('td');
-  dataElement.textContent = this.childName;
-  dataElement.setAttribute('class', 'childNameTable');
-  rowElement.appendChild(dataElement);
+var child1 = new ChildDailyData('Child1', 'Beach', 'Staff1');
+
+//end of DailyData constructor
+
+function makeHeaderRow() {
+  var tableRow = document.createElement('tr');
+  var thElement = document.createElement('th');
+  tableRow.appendChild(thElement);
+  for(var i = 0; i < locationsArray.length; i++) {
+    thElement = document.createElement('th');
+    thElement.textContent = locationsArray[i];
+    tableRow.appendChild(thElement);
+  }
+  tableRow = document.createElement('tr');
+  tableRow.textContent = 'Staff';
+  addChildInfoTable.appendChild(tableRow);
 }
 
+function makeChildRow() {
+  var tableRow;
+  var tdElement;
+  for(var child = 0; child < childArray.length; child++) {
+    tableRow = document.createElement('tr');
+    tdElement = document.createElement('td');
+    tdElement.textContent = childArray[child].locationsArray[i];
+    tableRow.appendChild(tdElement);
+  }
+}
 
+function handleChildDataSubmit(event) {
+  //prevent reload upon submit
+  event.preventDefault();
 
+  var inputChildData = event.target.addChildForm;
+  console.log('new child data submitted');
+}
+  makeHeaderRow();
+  makeChildRow();
+addChildForm.addEventListener('submit', handleChildDataSubmit);
 
-
-
-
-
-
-
-
-
-
-
-
-//   if(document.getElementById('radio1beach').click === true) {
-//     console.log('beach');
-//   }else if(document.getElementById('radio1treehouse').click === true) {
-//     console.log('treehouse');
-//   } else if (document.getElementById('radio1forest').click === true) {
-//     console.log('forest');
-//   } else {
-//     //console.log('no location selected');
+//
+// function makeStaffRow() {
+//   var rowElement = document.createElement('tr');
+//   var dataElement = document.createElement('td');
+//   dataElement.textContent = 'Staff';
+//   dataElement.setAttribute('class', 'addChildInfo');
+//   rowElement.appendChild(dataElement);
+//   for( var i = 0; i < locationsArray.length; i++) {
+//     for(var j = 0; j < childArray.length; j++) {
+//
+//     }
 //   }
 // }
-//
-// function uncheck() {
-//     document.getElementById('radio1beach').click === false;
-// }
-//
-// window.onload = function() {
-//
-//         var child1 = document.getElementById('radio1beach');
-//         var child2 = document.getElementById('radio2beach');
-//         var child3 = document.getElementById('radio3beach');
-//
-//         child1.onclick = handler;
-//         child2.onclick = handler;
-//         child3.onclick = handler;
-//
-//     }
-//
-//     function handler() {
-//         console.log('clicked');
-//     }
+
+//Event handling for form will prob need a prevent default here
+
+// makeHeaderRow(addChildInfo);
+// makeStaffRow();
+
+
+
+
+
+  //
+  //
+  //
+  // if(document.getElementById('beach').click === true) {
+  //   console.log('beach');
+  // }else if(document.getElementById('treehouse').click === true) {
+  //   console.log('treehouse');
+  // } else if (document.getElementById('forest').click === true) {
+  //   console.log('forest');
+  // } else {
+  //   //console.log('no location selected');
+  // }
