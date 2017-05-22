3cf0ed9b0902df2c81735a2ff3f8add24b60cb19
THXBurke <rachelwheels@gmail.com>
2017-05-22 00:29:18 -0700

    table header working

--- app.js
+++ app.js
@@ -8,85 +8,38 @@ var addChildForm = document.getElementById('addChildForm');
 var addChildInfoTable = document.getElementById('addChildInfoTable');
 //var staffLocation = document.getElementById('addStaffLocation');
 
-//DailyData constructor
-function ChildDailyData(childName, location, staff) {
-  this.childName = childName;
-  this.location = location;
-  this.staff = staff;
-}
-
-var child1 = new ChildDailyData('Child1', 'Beach', 'Staff1');
-
-//end of DailyData constructor
+var headerArray = ['Name', 'Beach', 'Treehouse', 'Forest'];
 
 function makeHeaderRow() {
   var tableRow = document.createElement('tr');
   var thElement = document.createElement('th');
-  tableRow.appendChild(thElement);
-  for(var i = 0; i < locationsArray.length; i++) {
+  for (var i = 0; i < headerArray.length; i++) {
     thElement = document.createElement('th');
-    thElement.textContent = locationsArray[i];
+    thElement.textContent = headerArray[i];
     tableRow.appendChild(thElement);
   }
-  tableRow = document.createElement('tr');
-  tableRow.textContent = 'Staff';
-  addChildInfoTable.appendChild(tableRow);
+  thElement = document.createElement('th');
+  thElement.textContent = headerArray[i];
+  addChildInfoTable.appendChild(tableRow);;
 }
+makeHeaderRow();
 
-function makeChildRow() {
-  var tableRow;
-  var tdElement;
-  for(var child = 0; child < childArray.length; child++) {
-    tableRow = document.createElement('tr');
-    tdElement = document.createElement('td');
-    tdElement.textContent = childArray[child].locationsArray[i];
-    tableRow.appendChild(tdElement);
-  }
-}
 
-function handleChildDataSubmit(event) {
-  //prevent reload upon submit
-  event.preventDefault();
 
-  var inputChildData = event.target.addChildForm;
-  console.log('new child data submitted');
-}
-  makeHeaderRow();
-  makeChildRow();
-addChildForm.addEventListener('submit', handleChildDataSubmit);
 
+// function displayTable() {
+//   var container = document.getElementById('container');
+//   //create table element
+//   var table = document.createElement('table');
+//   var tbody = document.createElement('tbody');
+//   var thead = document.createElement('thead');
 //
-// function makeStaffRow() {
-//   var rowElement = document.createElement('tr');
-//   var dataElement = document.createElement('td');
-//   dataElement.textContent = 'Staff';
-//   dataElement.setAttribute('class', 'addChildInfo');
-//   rowElement.appendChild(dataElement);
-//   for( var i = 0; i < locationsArray.length; i++) {
-//     for(var j = 0; j < childArray.length; j++) {
+//   table.appendChild(thead);
 //
-//     }
-//   }
+// for (var i = 0; i < headerArray.length; i++) {
+//   tHead.appendChild(document.createElement('th')).
+//   appendChild(document.createTextNode(headerArray[i]));
 // }
-
-//Event handling for form will prob need a prevent default here
-
-// makeHeaderRow(addChildInfo);
-// makeStaffRow();
-
-
-
-
-
-  //
-  //
-  //
-  // if(document.getElementById('beach').click === true) {
-  //   console.log('beach');
-  // }else if(document.getElementById('treehouse').click === true) {
-  //   console.log('treehouse');
-  // } else if (document.getElementById('forest').click === true) {
-  //   console.log('forest');
-  // } else {
-  //   //console.log('no location selected');
-  // }
+// //loop array that will push into childArray with form
+// displayTable();
+// }
